f3d93faa730dd7cebc12de191da278ac446737b0
Alex Beeken <aeb242@gmail.com>
2017-06-08 20:56:24 -0700

    add border and photo links to bottom of the page

--- app/controllers/tufa.js
+++ app/controllers/tufa.js
@@ -5,29 +5,29 @@ const { computed } = Ember;
 export default Ember.Controller.extend({
   slides: [
     {
-      title: 'Example Title',
-      subtitle: 'example subtitle',
+      title: 'Example Title 1',
+      subtitle: 'example subtitle 1',
       data: '1234 units (example data)',
       description: 'Long description with interpretation or definition or any other relevent information.',
       path: '/negit-1993.jpg'
     },
     {
-      title: 'Example Title',
-      subtitle: 'example subtitle',
+      title: 'Example Title 2',
+      subtitle: 'example subtitle 2',
       data: '1234 units (example data)',
       description: 'Long description with interpretation or definition or any other relevent information.',
       path: '/negit-1998.jpg'
     },
     {
-      title: 'Example Title',
-      subtitle: 'example subtitle',
+      title: 'Example Title 3',
+      subtitle: 'example subtitle 3',
       data: '1234 units (example data)',
       description: 'Long description with interpretation or definition or any other relevent information.',
       path: '/negit-2005.jpg'
     },
     {
-      title: 'Example Title',
-      subtitle: 'example subtitle',
+      title: 'Example Title 4',
+      subtitle: 'example subtitle 4',
       data: '1234 units (example data)',
       description: 'Long description with interpretation or definition or any other relevent information.',
       path: '/negit-2009.jpg'
@@ -38,8 +38,8 @@ export default Ember.Controller.extend({
     return this.get('slides')[this.get('currentSlideIdx')]
   }),
   actions: {
-    switchIdx: function(Idx) {
-      this.set('currentSlideIdx', Idx);
+    switchIdx: function(idx) {
+      this.set('currentSlideIdx', idx);
     }
   }
 })
