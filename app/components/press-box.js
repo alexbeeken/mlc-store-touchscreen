76f63d2fa98ce5fa08428c8c30ec52382b6cf34f
Alex Beeken <aeb242@gmail.com>
2017-06-09 07:52:27 -0700

    add background image to touch links

--- app/components/press-box.js
+++ app/components/press-box.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
+  backgroundImage: computed('currentSlide', function() {
+    return "background-image: "
+      + "url('"
+      + this.get('imagePath')
+      + "');"
+  }),
 });
