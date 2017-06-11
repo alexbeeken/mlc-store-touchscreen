94118eb04a9632774d0f26f410e276e5641c5abb
Alex Beeken <aeb242@gmail.com>
2017-06-10 17:02:38 -0700

    take out unused fields

--- app/controllers/tufa.js
+++ app/controllers/tufa.js
@@ -6,37 +6,25 @@ export default Ember.Controller.extend({
       year: '1962',
       levelMeter: '1948.9 m',
       levelFeet: '6394 ft',
-      description: '',
-      path: '/tufa_1962.jpg',
-      height: '768',
-      width: '1024'
+      path: '/tufa_1962.jpg'
     },
     {
       year: '1968',
       levelMeter: '1946.8 m',
       levelFeet: '6387 ft',
-      description: '',
-      path: '/tufa_1968.jpg',
-      height: '768',
-      width: '1024'
+      path: '/tufa_1968.jpg'
     },
     {
       year: '1995',
       levelMeter: '1944 m',
       levelFeet: '6378 ft',
-      description: '',
-      path: '/tufa_1995.jpg',
-      height: '768',
-      width: '1024'
+      path: '/tufa_1995.jpg'
     },
     {
       year: '2008',
       levelMeter: '1945.5 m',
       levelFeet: '6383 ft',
-      description: '',
-      path: '/tufa_2008.jpg',
-      height: '768',
-      width: '1024'
+      path: '/tufa_2008.jpg'
     }
   ]
 })
