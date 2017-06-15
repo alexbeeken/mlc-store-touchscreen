4452d5fbbfb5262b7b2cf7cd3d47e36ad742d058
Alex Beeken <aeb242@gmail.com>
2017-06-15 10:45:36 -0700

    make index links responsive - still in progress

--- app/controllers/tufa.js
+++ app/controllers/tufa.js
@@ -1,30 +1,9 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Controller.extend({
-  slides: [
-    {
-      year: '1962',
-      levelMeter: '1948.9 m',
-      levelFeet: '6394 ft',
-      path: '/tufa_1962.jpg'
-    },
-    {
-      year: '1968',
-      levelMeter: '1946.8 m',
-      levelFeet: '6387 ft',
-      path: '/tufa_1968.jpg'
-    },
-    {
-      year: '1995',
-      levelMeter: '1944 m',
-      levelFeet: '6378 ft',
-      path: '/tufa_1995.jpg'
-    },
-    {
-      year: '2008',
-      levelMeter: '1945.5 m',
-      levelFeet: '6383 ft',
-      path: '/tufa_2008.jpg'
-    }
-  ]
+  slides: function() {
+      var xml = $.parseXML('content/tufa_benchmark.xml');
+  }
 })
