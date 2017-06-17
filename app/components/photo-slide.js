6e8771ac3a9cc108907f31c23597d53463efff3c
Alex Beeken <aeb242@gmail.com>
2017-06-16 17:13:43 -0700

    automatically transition between photo slides

--- app/components/photo-slide.js
+++ app/components/photo-slide.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 const { computed } = Ember;
 
+const slideInterval = 8000;
+
 export default Ember.Component.extend({
   slides: [],
   currentSlideIdx: 0,
@@ -19,9 +21,21 @@ export default Ember.Component.extend({
       + this.get('currentSlide.width')
       + 'px;'
   }),
+  init: function() {
+    this._super();
+    this.send('startSwitching');
+  },
   actions: {
     switchIdx: function(idx) {
       this.set('currentSlideIdx', idx);
+    },
+    startSwitching: function(idx = 0) {
+        Ember.run.later(this, function() {
+          var currentIdx = this.get('currentSlideIdx')
+          var newIdx = (currentIdx + 1) % 4
+          this.send('switchIdx', (currentIdx + 1) % 4)
+          this.send('startSwitching', newIdx)
+        }, slideInterval)
+      }
     }
-  }
-});
+  });
