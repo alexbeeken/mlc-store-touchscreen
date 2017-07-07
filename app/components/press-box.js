1c5a6b9bba4c4d2021b5dd2c10cbe6ea0fb01988
Alex Beeken <aeb242@gmail.com>
2017-07-07 10:14:44 -0700

    add styling for mobile devices on index page

--- app/components/press-box.js
+++ app/components/press-box.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
+const { computed, inject } = Ember;
+const { service } = inject;
 
 export default Ember.Component.extend({
+  media: inject.service(),
   backgroundImage: computed('currentSlide', function() {
     return "background-image: "
       + "url('"
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,11 +3,13 @@ const { computed } = Ember;
 import { content as negitContent } from '../content/negit';
 import { content as tufaContent } from '../content/tufa_benchmark';
 
+
 export default Ember.Controller.extend({
   links: computed( function() {
     return [
       negitContent,
-      tufaContent
+      tufaContent,
+      negitContent
     ]
   })
 })
