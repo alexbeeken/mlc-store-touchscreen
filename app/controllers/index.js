37e83cfefc9f947aee68716a13aa20118193a4d4
Alex Beeken <aeb242@gmail.com>
2017-07-09 10:14:54 -0700

    return to normal number of menu items

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,21 +4,19 @@ const { service } = inject;
 import { content as negitContent } from '../content/negit';
 import { content as tufaContent } from '../content/tufa_benchmark';
 
-
 export default Ember.Controller.extend({
   screen: inject.service(),
   media: inject.service(),
   links: computed( function() {
     return [
       negitContent,
-      tufaContent,
-      negitContent
+      tufaContent
     ]
   }),
   showForwardArrow: computed( function() {
-    return !this.get('media.isMobile')
+    return !this.get('media.isMobile') && this.get('links').length > 3
   }),
   showBackArrow: computed( function() {
-    return !this.get('media.isMobile')
+    return !this.get('media.isMobile') && this.get('links').length > 3
   })
 })
