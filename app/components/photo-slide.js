2ff647e5dbbdebe730f535e45fbb247dc60af464
Alex Beeken <aeb242@gmail.com>
2017-07-16 09:31:00 -0700

    attempt to reset slide switch timer but not sure it works as intended

--- app/components/photo-slide.js
+++ app/components/photo-slide.js
@@ -11,23 +11,31 @@ export default Ember.Component.extend({
   slides: [],
   currentSlideIdx: 0,
   currentSlide: null,
+  switchReturnValue: null,
   init: function() {
     this._super();
     this.send('startSwitching');
   },
   actions: {
     switchIdx: function(idx) {
-      this.set('currentSlideIdx', idx);
+      this.set('currentSlideIdx', idx)
       this.set('currentSlide', this.get('slides')[idx])
+    },
+    switchIdxManually: function(idx) {
+      this.set('currentSlideIdx', idx)
+      this.set('currentSlide', this.get('slides')[idx])
+      Ember.run.cancel(this.get('switchReturnValue'))
+      this.send('startSwitching')
     },
     startSwitching: function() {
       if (!this.get('media.isMobile')) {
-        Ember.run.later(this, function() {
+        var returnValue = Ember.run.later(this, function() {
             var currentIdx = this.get('currentSlideIdx')
             var newIdx = (currentIdx + 1) % 4
             this.send('switchIdx', (currentIdx + 1) % 4)
             this.send('startSwitching', newIdx)
         }, slideInterval)
+        this.set('switchReturnValue', returnValue)
       }
     }
   }
