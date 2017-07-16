5feb4f241bbbf3f06d49c6a38501b91c14c5767f
Alex Beeken <aeb242@gmail.com>
2017-07-16 09:05:35 -0700

    instantiate photo model wth content

--- app/controllers/negit.js
+++ app/controllers/negit.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 import { content } from '../content/negit';
+const { computed } = Ember;
 
 export default Ember.Controller.extend({
-  slides: content.slides
+  slides: computed( function() {
+    var photos = []
+    content.slides.forEach( (photo) => {
+      photos.push(
+        this.get('store').createRecord('photo', {
+          year: photo.year,
+          levelMeter: photo.levelMeter,
+          levelFeet: photo.levelFeet,
+          path: photo.path
+        })
+      )
+    })
+    return photos
+  })
 })
