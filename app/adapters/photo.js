1b24be0018a11c3cc0e222c7c4bda6f951496623
Alex Beeken <aeb242@gmail.com>
2017-07-21 14:57:23 -0700

    generate ids for models

---
+++ app/adapters/photo.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.JSONAPIAdapter.extend({
+  counter: 0,
+  generateIdForRecord: function() {
+    var counter = this.get('counter')
+    this.set('counter', counter + 1)
+    return counter
+  }
+});
