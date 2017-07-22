8407926c0f3e5d180a47543a21c15e66ef06bd12
Alex Beeken <aeb242@gmail.com>
2017-07-21 20:58:56 -0700

    remove unused files

--- app/controllers/exhibit.js
+++ app/controllers/exhibit.js
@@ -3,11 +3,4 @@ const { computed, inject } = Ember;
 const { service } = inject;
 
 export default Ember.Controller.extend({
-  store: service(),
-  slides: computed('model', function() {
-    return this.get('model.photos').toArray()
-  }),
-  firstSlide: computed( function() {
-    return this.get('slides')[0]
-  })
 })
