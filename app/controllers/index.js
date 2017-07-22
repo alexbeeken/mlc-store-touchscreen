c9adc357b3aa739a801646d73f5fb4f7e71a930b
Alex Beeken <aeb242@gmail.com>
2017-07-22 06:52:25 -0700

    paginate index with arrows - needs styling and animations

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,15 +3,31 @@ const { computed, inject } = Ember;
 const { service } = inject;
 
 export default Ember.Controller.extend({
-  screen: service(),
   media: service(),
   exhibits: computed( function() {
-    return this.store.peekAll('exhibit')
+    return this.store.peekAll('exhibit').toArray()
   }),
-  showForwardArrow: computed( function() {
-    return !this.get('media.isMobile') && this.get('exhibits').length > 3
+  showingExhibits: computed('currentShowingIdx', function() {
+    var idx = this.get('currentShowingIdx')
+    return this.get('exhibits').slice(idx, idx+3)
   }),
-  showBackArrow: computed( function() {
-    return !this.get('media.isMobile') && this.get('exhibits').length > 3
-  })
+  currentShowingIdx: 0,
+  showForwardArrow: computed('currentShowingIdx', function() {
+    return !this.get('media.isMobile')
+    && this.get('currentShowingIdx') < (this.get('exhibits').length - 3)
+    && this.get('exhibits').length > 2
+  }),
+  showBackArrow: computed('currentShowingIdx', function() {
+    return !this.get('media.isMobile')
+    && this.get('currentShowingIdx') > 0
+    && this.get('exhibits').length > 2
+  }),
+  actions: {
+    changeIdx: function(change) {
+      var idx = this.get('currentShowingIdx')
+      var newIdx = idx+change
+      this.set('currentShowingIdx', newIdx)
+      console.log(this.get('currentShowingIdx'))
+    }
+  }
 })
