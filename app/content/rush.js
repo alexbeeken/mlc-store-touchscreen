8d640732a3050d8e41f204f3f6e72b0d4e49f7df
Alex Beeken <aeb242@gmail.com>
2017-07-23 14:27:04 -0700

    Create rush.js

---
+++ app/content/rush.js
@@ -0,0 +1,23 @@
+export const rush =
+{
+  title: 'Rush Creek',
+  route: 'rush',
+  linkColor: '#D3D3D3',
+  slides: [
+    {
+      year: 'May 2, 1948',
+      levelMeter: '',
+      levelFeet: 'People fishing on Lower Rush Creek on May 2, 1948. At least 11 anglers are visible in this photograph.
+May 2, 1948 flow below Grant Lake Reservoir: 0 cfs.',
+      path: '/vestal61948.jpg'
+    },
+    {
+      year: 'April 10, 1947',
+      levelMeter: '',
+      levelFeet: 'A weir and fish trap on Rush Creek before entering the delta on April 10, 1947. Mono Lake is in the background behind the building. This building was used by duck hunters.
+April 1947 average flow below Grant Lake Reservoir: 49 cfs
+April 1947 average flow at highway: 12 cfs',
+      path: '/vestal41947.jpg'
+    },
+  ]
+}
