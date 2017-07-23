54cdc86303a546af70242bf90a0fedb4d8a96685
Alex Beeken <aeb242@gmail.com>
2017-07-23 14:38:33 -0700

    Update index.js

--- app/content/index.js
+++ app/content/index.js
@@ -1,9 +1,10 @@
 import { negit } from '../content/negit';
 import { tufa } from '../content/tufa_benchmark';
+import { rush } from '../content/rush';
 
 export const defaults =
   {
     title: "Mono Lake's Changing Water Levels",
     home: false,
-    exhibits: [negit, tufa]
+    exhibits: [negit, tufa, rush]
   }
