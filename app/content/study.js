37e25295983e797d95750ab4fbe6b928d8f647f7
Alex Beeken <aeb242@gmail.com>
2017-07-24 08:31:41 -0700

    Create study.js

---
+++ app/content/study.js
@@ -0,0 +1,93 @@
+export const negit =
+{
+  title: '1976 Ecological Study',
+  route: 'study',
+  linkColor: '#db7d2b',
+  slides: [
+    {
+      year: '1976',
+      paragraph: 'Top: Elliot Burch; second row: Christine Weigen and Gayle Dana; third row: Tom Wainwright, visitor Bill Syme and Brett Engstrom; bottom row: Bob Loeffler, Jeff Burch, David Winkler and Dave Herbst. Jeff Robbins is standing at left.',
+      path: '/1976researcherssm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'David Winkler and Elliot Burch with bicycles.',
+      path: '/1976dwwinkelliotbikessm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'Photo of Thomas Wainwright during a plant survey with transit in background.',
+      path: '/1976twplantsurveysm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'John writes: A rattlesnake picked up in camp. Jeff Robins was sitting in a lawn chair reading when this snake approached him; he got up and moved off, and the snake went into someone's tent and crawled under the sleeping bag. The fellow holding the snake was someone from the chipmunk study; they also stayed on the creek in 1975 and 1976. I think his name was Tom, but I'm not sure.',
+      path: '/1976jhfangsm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'John writes: The throne was constructed out in the sagebrush. I think a bird list was kept; in order to qualify for the list, the bird had to be seen while one's posterior was in contact with the seat.',
+      path: '/1976jhthronesm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'Phalarope banding: Dave Winkler and Jeff Robins.',
+      path: '/1976jhbanding2sm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'Phalarope banding: Dave Winkler and Christine Weigen.',
+      path: '/1976jhbanding3sm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'Phalarope banding: Several unidentified people tending a net.',
+      path: '/1976jhbanding4.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'Phalarope banding: Dave Winkler and Jeff Robins.',
+      path: '/1976jhbanding2sm.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: 'J. Burch and T. Wainwright walk on the east side of Mono Lake.',
+      path: '/1976jburchtwaineastsidemono.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976monolake1.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976monolake6.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976monolake3.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976monolake3.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976monolake5.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976monofloat4july.jpg'
+    },
+    {
+      year: '1976',
+      paragraph: ' ',
+      path: '/1976tiogalodge.jpg'
+    },
+  ]
+}
