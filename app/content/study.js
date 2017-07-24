3e57279928d45b36cf0269fd6122a5bcd864ce1d
Alex Beeken <aeb242@gmail.com>
2017-07-24 08:41:51 -0700

    fix broken photo links

--- app/content/study.js
+++ app/content/study.js
@@ -1,4 +1,4 @@
-export const negit =
+export const study =
 {
   title: '1976 Ecological Study',
   route: 'study',
@@ -7,37 +7,37 @@ export const negit =
     {
       year: '1976',
       paragraph: 'Top: Elliot Burch; second row: Christine Weigen and Gayle Dana; third row: Tom Wainwright, visitor Bill Syme and Brett Engstrom; bottom row: Bob Loeffler, Jeff Burch, David Winkler and Dave Herbst. Jeff Robbins is standing at left.',
-      path: '/1976researcherssm.jpg'
+      path: '/1976researchers.jpg'
     },
     {
       year: '1976',
       paragraph: 'David Winkler and Elliot Burch with bicycles.',
-      path: '/1976dwwinkelliotbikessm.jpg'
+      path: '/1976dwwinkelliotbikes.jpg'
     },
     {
       year: '1976',
       paragraph: 'Photo of Thomas Wainwright during a plant survey with transit in background.',
-      path: '/1976twplantsurveysm.jpg'
+      path: '/1976twplantsurvey.jpg'
     },
     {
       year: '1976',
       paragraph: "John writes: A rattlesnake picked up in camp. Jeff Robins was sitting in a lawn chair reading when this snake approached him; he got up and moved off, and the snake went into someone's tent and crawled under the sleeping bag. The fellow holding the snake was someone from the chipmunk study; they also stayed on the creek in 1975 and 1976. I think his name was Tom, but I'm not sure.",
-      path: '/1976jhfangsm.jpg'
+      path: '/1976jhfang.jpg'
     },
     {
       year: '1976',
       paragraph: "John writes: The throne was constructed out in the sagebrush. I think a bird list was kept; in order to qualify for the list, the bird had to be seen while one's posterior was in contact with the seat.",
-      path: '/1976jhthronesm.jpg'
+      path: '/1976jhthrone.jpg'
     },
     {
       year: '1976',
       paragraph: 'Phalarope banding: Dave Winkler and Jeff Robins.',
-      path: '/1976jhbanding2sm.jpg'
+      path: '/1976jhbanding2.jpg'
     },
     {
       year: '1976',
       paragraph: 'Phalarope banding: Dave Winkler and Christine Weigen.',
-      path: '/1976jhbanding3sm.jpg'
+      path: '/1976jhbanding3.jpg'
     },
     {
       year: '1976',
@@ -47,7 +47,7 @@ export const negit =
     {
       year: '1976',
       paragraph: 'Phalarope banding: Dave Winkler and Jeff Robins.',
-      path: '/1976jhbanding2sm.jpg'
+      path: '/1976jhbanding2.jpg'
     },
     {
       year: '1976',
