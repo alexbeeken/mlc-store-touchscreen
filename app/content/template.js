9f5c979fd61a53b21cc32043af6f406db37628f5
monolakecommittee <30573351+monolakecommittee@users.noreply.github.com>
2017-07-30 08:20:27 -0700

    Update template.js

--- app/content/template.js
+++ app/content/template.js
@@ -1,1 +1,19 @@
 
+export const example =
+{
+  title: 'Example Title',
+  route: 'example',
+  linkColor: '#FFFFFF',
+  slides: [
+    {
+      year: '2017',
+      paragraph: 'Example text',
+      path: '/example.jpg'
+    },
+    {
+      year: '2017',
+      paragraph: 'Example text 2',
+      path: '/example2s.jpg'
+    }
+  ]
+}
